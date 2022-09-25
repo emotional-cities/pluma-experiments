4856f337c22e771c29b8e47844bfcd4312a82d54
bruno-f-cruz <bfpcruz@gmail.com>
2022-09-25 20:54:01 +0100

    add UBX, Aceelerometer, Empatica and Microphone stream classes

--- 20092022/Analysis/AnalysisCode/utils/EmotionalCitiesStreams.py
+++ 20092022/Analysis/AnalysisCode/utils/EmotionalCitiesStreams.py
@@ -1,7 +1,6 @@
 from os import device_encoding
 import pandas as pd
-from utils.dataloader import load_harp_stream
-
+from utils.dataloader import load_harp_stream, load_ubx_stream, load_accelerometer, load_empatica, load_microphone
 
 class Stream:
     def __init__(self, device, streamlabel, root = '', data = None, autoload = True):
@@ -20,9 +19,38 @@ class HarpStream(Stream):
 
 	def load(self):
 		self.data = load_harp_stream(self.eventcode, root = self.rootfolder, throwFileError=False)
+class UbxStream(Stream):
+	def __init__(self, **kw):
+		super(UbxStream,self).__init__(**kw)
+		if self.autoload:
+			self.load()
 
-# class UbxStream:
-#     class B(A):
-#     def __init__(self,b,**kw):
-#         self.b=b
-#         super(B,self).__init__(**kw)
+	def load(self):
+		self.data = load_ubx_stream(root = self.rootfolder)
+
+class AccelerometerStream(Stream):
+	def __init__(self, **kw):
+		super(AccelerometerStream,self).__init__(**kw)
+		if self.autoload:
+			self.load()
+
+	def load(self):
+		self.data = load_accelerometer(root = self.rootfolder)
+
+class MicrophoneStream (Stream):
+	def __init__(self, **kw):
+		super(MicrophoneStream,self).__init__(**kw)
+		if self.autoload:
+			self.load()
+
+	def load(self):
+		self.data = load_microphone(root = self.rootfolder)
+
+class EmpaticaStream(Stream):
+	def __init__(self, **kw):
+		super(EmpaticaStream,self).__init__(**kw)
+		if self.autoload:
+			self.load()
+
+	def load(self):
+		self.data = load_empatica(root = self.rootfolder)
