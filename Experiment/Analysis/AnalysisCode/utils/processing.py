1319b188dc32700d6a04ac77818698b0e3592ea5
bruno-f-cruz <bfpcruz@gmail.com>
2022-10-21 08:37:03 +0100

    allow ECG raw signal inversion

--- Experiment/Analysis/AnalysisCode/utils/processing.py
+++ Experiment/Analysis/AnalysisCode/utils/processing.py
@@ -23,10 +23,12 @@ def normalize_data(data_in):
 
 def heartrate_from_ecg(ecg_data_stream,
                        fs = 250, skip_slice = 4, max_heartrate_bpm = 200.0,
-                       peak_height = 800, smooth_win = 10):
+                       peak_height = 800, smooth_win = 10, invert = False):
     ## Load biodata
 
     ecg = ecg_data_stream.data
+    if invert:
+        ecg = ecg * (-1.0)
     ecg = ecg["Value0"].iloc[np.arange(len(ecg))[::skip_slice]].astype(np.float64) # sensor acquires at 250hz but saves at 1khz
 
     resampled = butter_highpass_filter(ecg,5,fs)
@@ -39,4 +41,4 @@ def heartrate_from_ecg(ecg_data_stream,
     heartrate[-int(smooth_win/2)-1:-1] = np.NaN
     df = pd.DataFrame(index = ecg.index[peaks[1:]], copy = True)
     df['HeartRate'] = heartrate
-    return df
+    return (df, resampled)
