52fe142b668cafbdd540aeae87c60619f5cb0c97
glopesdev <glopesdev@users.noreply.github.com>
2023-09-07 13:42:37 +0100

    Improve status display layout and add label

--- Workflows/Extensions/StreamingStatusVisualizer.cs
+++ Workflows/Extensions/StreamingStatusVisualizer.cs
@@ -1,4 +1,5 @@
 ﻿using Bonsai.Design;
+using Bonsai.Expressions;
 using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -6,9 +7,18 @@ using System.Windows.Forms;
 
 public class StreamingStatusVisualizer : DialogTypeVisualizer
 {
+    static readonly StringFormat LabelFormat = new StringFormat(StringFormat.GenericDefault)
+    {
+        Alignment = StringAlignment.Center,
+        LineAlignment = StringAlignment.Center,
+        FormatFlags = StringFormatFlags.NoWrap,
+        Trimming = StringTrimming.Character
+    };
+
     bool? value;
     Panel lightPanel;
     Pen lightPen;
+    Font labelFont;
 
     public override void Show(object value)
     {
@@ -18,8 +28,13 @@ public class StreamingStatusVisualizer : DialogTypeVisualizer
 
     public override void Load(IServiceProvider provider)
     {
+        var context = (ITypeVisualizerContext)provider.GetService(typeof(ITypeVisualizerContext));
+        var combinatorBuilder = ExpressionBuilder.GetVisualizerElement(context.Source).Builder;
+        var streamingStatus = (StreamingStatus)ExpressionBuilder.GetWorkflowElement(combinatorBuilder);
+
         lightPen = new Pen(Brushes.Black, 4);
         lightPanel = new LightPanel();
+        labelFont = new Font(lightPanel.Font, FontStyle.Bold);
         lightPanel.Dock = DockStyle.Fill;
         lightPanel.Resize += delegate { lightPanel.Invalidate(); };
         lightPanel.Paint += (sender, e) =>
@@ -32,6 +47,15 @@ public class StreamingStatusVisualizer : DialogTypeVisualizer
             rectangle.Inflate((int)-lightPen.Width, (int)-lightPen.Width);
             e.Graphics.FillEllipse(brush, rectangle);
             e.Graphics.DrawEllipse(lightPen, rectangle);
+            if (!string.IsNullOrEmpty(streamingStatus.Name))
+            {
+                e.Graphics.DrawString(
+                    streamingStatus.Name,
+                    labelFont,
+                    Brushes.White,
+                    rectangle,
+                    LabelFormat);
+            }
         };
 
         var visualizerService = (IDialogTypeVisualizerService)provider.GetService(typeof(IDialogTypeVisualizerService));
@@ -44,7 +68,11 @@ public class StreamingStatusVisualizer : DialogTypeVisualizer
     public override void Unload()
     {
         lightPanel.Dispose();
+        lightPen.Dispose();
+        labelFont.Dispose();
         lightPanel = null;
+        lightPen = null;
+        labelFont = null;
     }
 
     class LightPanel : Panel
