81b873bc68bbba2104685262aeeb25eefcdaf14d
glopesdev <glopesdev@users.noreply.github.com>
2023-09-07 11:46:24 +0100

    Ensure any whitespace is removed

--- Workflows/Extensions/EnsureIdentifier.cs
+++ Workflows/Extensions/EnsureIdentifier.cs
@@ -6,18 +6,21 @@ using System.Reactive.Linq;
 [Combinator]
 [Description("Ensure that all strings in the sequence are not null or empty.")]
 [WorkflowElementCategory(ElementCategory.Combinator)]
-public class AssertNotNullOrEmpty
+public class EnsureIdentifier
 {
+    [Description("The message to display in case of validation errors.")]
     public string Message { get; set; }
 
     public IObservable<string> Process(IObservable<string> source)
     {
-        return source.Do(value =>
+        return source.Select(value =>
         {
             if (string.IsNullOrEmpty(value))
             {
                 throw new InvalidOperationException(Message);
             }
+
+            return value.Replace(" ", string.Empty);
         });
     }
 }
