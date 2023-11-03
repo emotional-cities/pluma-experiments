5692db8e12ba7a3c136c7a8757f840fe3a0c7b4a
Your Name <you@example.com>
2023-11-03 00:53:36 +0000

    Publish only the timestamps for each render frame

--- VR-Alfama/Assets/Scripts/ScreenPublisher.cs
+++ VR-Alfama/Assets/Scripts/ScreenPublisher.cs
@@ -21,25 +21,9 @@ public class ScreenPublisher : DataPublisher
         {
             yield return new WaitForEndOfFrame();
             long timestamp = DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond / 1000);
-           
 
-            // Do publishing
-            
-
-            //byte[] imagetoSend = ScreenCapture.CaptureScreenshotAsTexture().EncodeToJPG();
-            //
-            Texture2D screenShot = new Texture2D(Screen.width, Screen.height);
-            screenShot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-            screenShot.Apply();
-            byte[] bytes = screenShot.EncodeToJPG();
-            UnityEngine.Object.Destroy(screenShot);
             PubSocket.SendMoreFrame("ScreenShot")
-                .SendMoreFrame(BitConverter.GetBytes(timestamp))
-                .SendFrame(bytes);
-            yield return new WaitForSecondsRealtime(0.05f);
-            //for (int i = 0; i < 15; i++)
-            //    yield return null;
-            yield return null;
+                .SendFrame(BitConverter.GetBytes(timestamp));
         }
 
 
