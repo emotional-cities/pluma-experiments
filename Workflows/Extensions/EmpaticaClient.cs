9ae72a0ce445b0e205cc9d7315c7a1dd868669a5
glopesdev <glopesdev@users.noreply.github.com>
2024-04-02 15:25:24 +0100

    Replace pupil interface and empatica packages

--- Workflows/Extensions/EmpaticaClient.cs
+++ Workflows/Extensions/EmpaticaClient.cs
@@ -1,3 +1,4 @@
+using Bonsai;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,65 +12,62 @@ using System.Reactive.Disposables;
 using System.Threading;
 using System.IO;
 
-namespace Bonsai.WebSockets
+public class EmpaticaClient : Source<string>
 {
-    public class EmpaticaClient : Source<string>
+    public string Host { get; set; }
+    public int Port { get; set; }
+
+    public override IObservable<string> Generate()
     {
-        public string Host { get; set; }
-        public int Port { get; set; }
+        return Generate(null);
+    }
 
-        public override IObservable<string> Generate()
+    public IObservable<string> Generate(IObservable<string> messageSequence)
+    {
+        return Observable.Create<string>((observer, cancellationToken) =>
         {
-            return Generate(null);
-        }
+            var client = new TcpClient(Host, Port);
+            var stream = client.GetStream();
 
-        public IObservable<string> Generate(IObservable<string> messageSequence)
-        {
-            return Observable.Create<string>((observer, cancellationToken) =>
+            if (messageSequence != null)
             {
-                var client = new TcpClient(Host, Port);
-                var stream = client.GetStream();
-
-                if (messageSequence != null)
+                var messager = messageSequence.Do(message =>
                 {
-                    var messager = messageSequence.Do(message =>
-                    {
-                        byte[] writeBuffer = Encoding.ASCII.GetBytes(message + Environment.NewLine);
-                        stream.Write(writeBuffer, 0, writeBuffer.Length);
-                    }).Subscribe();
+                    byte[] writeBuffer = Encoding.ASCII.GetBytes(message + Environment.NewLine);
+                    stream.Write(writeBuffer, 0, writeBuffer.Length);
+                }).Subscribe();
 
-                    cancellationToken.Register(() => { messager.Dispose(); });
-                }
+                cancellationToken.Register(() => { messager.Dispose(); });
+            }
 
-                return Task.Factory.StartNew(() =>
+            return Task.Factory.StartNew(() =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
                 {
-                    while (!cancellationToken.IsCancellationRequested)
+                    if (stream.DataAvailable)
                     {
-                        if (stream.DataAvailable)
+                        byte[] readBuffer = new byte[client.Available];
+                        StringBuilder sb = new StringBuilder();
+                        int bytesRead = stream.Read(readBuffer, 0, readBuffer.Length);
+                        if (bytesRead > 0)
                         {
-                            byte[] readBuffer = new byte[client.Available];
-                            StringBuilder sb = new StringBuilder();
-                            int bytesRead = stream.Read(readBuffer, 0, readBuffer.Length);
-                            if (bytesRead > 0)
+                            var readString = Encoding.ASCII.GetString(readBuffer, 0, bytesRead);
+                            using (StringReader reader = new StringReader(readString))
                             {
-                                var readString = Encoding.ASCII.GetString(readBuffer, 0, bytesRead);
-                                using (StringReader reader = new StringReader(readString))
+                                string line;
+                                while ((line = reader.ReadLine()) != null)
                                 {
-                                    string line;
-                                    while ((line = reader.ReadLine()) != null)
-                                    {
-                                        observer.OnNext(line);
-                                    }
+                                    observer.OnNext(line);
                                 }
                             }
                         }
                     }
-                }).ContinueWith(task =>
-                {
-                    client.Dispose();
-                    task.Dispose();
-                });
+                }
+            }).ContinueWith(task =>
+            {
+                client.Dispose();
+                task.Dispose();
             });
-        }
+        });
     }
 }
