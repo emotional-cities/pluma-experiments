01dc314970ac470db360efe21ce35306d658f219
Andrew <erskine94@gmail.com>
2024-06-19 17:32:11 +0100

    Fix color format in map indicator

--- VR-Alfama/Assets/VrUtilities.cs
+++ VR-Alfama/Assets/VrUtilities.cs
@@ -8,24 +8,12 @@ using UnityEngine.Rendering.HighDefinition;
 
 public static class VrUtilities
 {
-    public static Texture2D TextureFromRenderTexture(RenderTexture renderTexture)
-    {
-        Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
-        //Graphics.CopyTexture(renderTexture, tex);
-
-        //return tex;
-
-        RenderTexture.active = renderTexture;
-
-        return tex;
-    }
-
     public static Texture2D TextureFromCamera(Camera camera)
     {
         int Width = camera.activeTexture.width;
         int Height = camera.activeTexture.height;
 
-        Texture2D target = new Texture2D(Width, Height, TextureFormat.RGB24, false);
+        Texture2D target = new Texture2D(Width, Height, TextureFormat.RGBAFloat, false);
 
         camera.Render();
 
