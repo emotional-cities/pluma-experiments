d47ab3ace08e6d5dc506cb66badafc8fb75176d6
aspaNeuro <a.almeida@neurogears.org>
2024-11-13 11:15:33 +0000

    Fix typos

--- Workflows/Extensions/ToUnityIti.cs
+++ Workflows/Extensions/ToUnityIti.cs
@@ -10,20 +10,20 @@ using OpenCV.Net;
 [Combinator]
 [Description("Get SecondsInterTrialInterval. From ZeroMQ Unity ITI Message")]
 [WorkflowElementCategory(ElementCategory.Transform)]
-public class ToUnityItI
+public class ToUnityIti
 {
     public IObservable<UnityIti> Process(IObservable<NetMQMessage> source)
     {
         return source.Select(value => new UnityIti
         {
             SystemTimeMicroseconds = BitConverter.ToInt64(value[1].Buffer, 0),
-            SecondsInterTrialInterval = BitConverter.ToSingle(value[2].Buffer, 0),
+            IntervalSeconds = BitConverter.ToSingle(value[2].Buffer, 0),
         });
     }
 }
 public class UnityIti
 {
     public long SystemTimeMicroseconds;
-    public float SecondsInterTrialInterval;
+    public float IntervalSeconds;
 
 }
